678e531407c22cf27997e077806e07ce18b7fb12
ZeusCoder <zeusselalusalah@gmail.com>
2022-04-15 09:47:35 +0700

    initial commit all

---
+++ models/Product.js
@@ -0,0 +1,43 @@
+const products = require("../data/products");
+
+class Product {
+  static findAll() {
+    return products;
+  }
+
+  static findById(id) {
+    const index = products.find((product) => product.id === id);
+    if (index) {
+      return index;
+    } else {
+      return `Product with id ${id} not found`;
+    }
+  }
+
+  static findOne(title) {
+    const filterProduct = products.filter((products) =>
+      products.name
+        .toLowerCase()
+        .replace(" ", "")
+        .includes(title.toLowerCase().replace(" ", ""))
+    );
+    if (filterProduct) {
+      return filterProduct;
+    } else {
+      `Product with title ${title} not found`;
+    }
+  }
+
+  static findByCategory(category) {
+    const allProducts = products.filter(
+      (product) => product.kategori === category
+    );
+    if (allProducts && allProducts.length > 0) {
+      return allProducts;
+    } else {
+      return `Product with category '${category}' not found`;
+    }
+  }
+}
+
+module.exports = Product;
---
+++ pages/api/v1/products/[pid].js
@@ -0,0 +1,9 @@
+import Product from "../../../../models/Product";
+
+export default function handler(req, res) {
+  const pid = req.query.pid;
+  const singleProdict = Product.findById(+pid);
+  res
+    .status(200)
+    .json({ message: "Success get data", products: singleProdict });
+}
