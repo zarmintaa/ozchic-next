94254a4701d6f1705e59fc654df288e4ccb95606
ZeusCoder <zeusselalusalah@gmail.com>
2022-06-08 21:51:53 +0700

    Update layout.js

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -10,7 +10,7 @@ const Layout = (props) => {
         <link
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
-          strategy="lazyOnload"
+          strategy="beforeInteractive"
         />
       </Head>
       <CartProvider>
