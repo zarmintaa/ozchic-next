df32e964be66c6aa819da1fd290be2ddd4c1a3b4
ZeusCoder <zeusselalusalah@gmail.com>
2022-06-11 22:10:14 +0700

    add logo

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="id">
-      <Head />
+      <Head>
+        <link rel="icon" href="/ozchic-logo.png" />
+      </Head>
       <body>
         <Main />
         <NextScript />
